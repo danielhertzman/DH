a8734d3b4ac062b9362cbea8f93e8422080f8ba3
Hinks <henrik.nr10@gmail.com>
2016-03-02 12:24:13 +0100

    Loop left.

--- src/dh/Interpreter.java
+++ src/dh/Interpreter.java
@@ -3,10 +3,12 @@ package dh;
 import dh.grammar.DhBaseListener;
 import dh.grammar.DhParser;
 import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.tree.ErrorNode;
 import org.antlr.v4.runtime.tree.TerminalNode;
 
 import java.util.HashMap;
+import java.util.Stack;
 
 public class Interpreter extends DhBaseListener{
 
@@ -14,148 +16,121 @@ public class Interpreter extends DhBaseListener{
 
     private final String infnam;
     private final HashMap<String, Var> vars = new HashMap<String, Var>();
-    private int exprVal;
+    private Stack<Integer> stack = new Stack<Integer>();
 
 
     public Interpreter(String infnam) {
         this.infnam = infnam;
     }
 
-    @Override public void enterCode(DhParser.CodeContext ctx) {
-        System.out.println("enter code" + ctx);
+    private Var getVar(Token tok) {
+
+        String name = tok.getText();
+        Var var = vars.get(name);
+
+        if (var == null) {
+
+            error(tok.getLine(), "undefined " + name);
+            return new Var();   // avoid null pointer exception
+
+        } else {
+            return var;
+        }
     }
-    /**
-     * {@inheritDoc}
-     *
-     * <p>The default implementation does nothing.</p>
-     */
-    @Override public void exitCode(DhParser.CodeContext ctx) {
-        System.out.println("exit code" + ctx);
+
+    private void error(int line, String msg) {
+        System.err.println(infnam + ":" + line + ": " + msg);
     }
-    /**
-     * {@inheritDoc}
-     *
-     * <p>The default implementation does nothing.</p>
-     */
-    @Override public void enterStatement(DhParser.StatementContext ctx) { }
-    /**
-     * {@inheritDoc}
-     *
-     * <p>The default implementation does nothing.</p>
-     */
-    @Override public void exitStatement(DhParser.StatementContext ctx) { }
-    /**
-     * {@inheritDoc}
-     *
-     * <p>The default implementation does nothing.</p>
-     */
-    @Override public void enterDecl(DhParser.DeclContext ctx) { }
-    /**
-     * {@inheritDoc}
-     *
-     * <p>The default implementation does nothing.</p>
-     */
-    @Override public void exitDecl(DhParser.DeclContext ctx) { }
-    /**
-     * {@inheritDoc}
-     *
-     * <p>The default implementation does nothing.</p>
-     */
-    @Override public void enterAssign(DhParser.AssignContext ctx) { }
-    /**
-     * {@inheritDoc}
-     *
-     * <p>The default implementation does nothing.</p>
-     */
-    @Override public void exitAssign(DhParser.AssignContext ctx) { }
-    /**
-     * {@inheritDoc}
-     *
-     * <p>The default implementation does nothing.</p>
-     */
-    @Override public void enterLoop(DhParser.LoopContext ctx) {
-        System.out.println("enter loop" + ctx);
+
+
+    @Override
+    public void enterStatement(DhParser.StatementContext ctx) { }
+
+    @Override
+    public void exitStatement(DhParser.StatementContext ctx) { }
+
+    @Override
+    public void enterDecl(DhParser.DeclContext ctx) {
+
+        String name = ctx.ID().getText();
+
+        Var old = vars.put(name, new Var());
+
+        if (old != null) {
+            error(ctx.ID().getSymbol().getLine(), "redefined " + name);
+        }
     }
-    /**
-     * {@inheritDoc}
-     *
-     * <p>The default implementation does nothing.</p>
-     */
-    @Override public void exitLoop(DhParser.LoopContext ctx) {
+
+    @Override
+    public void exitDecl(DhParser.DeclContext ctx) { }
+
+    @Override
+    public void enterAssign(DhParser.AssignContext ctx) {
+
+    }
+
+    @Override
+    public void exitAssign(DhParser.AssignContext ctx) {
+
+        getVar(ctx.ID().getSymbol()).val = stack.pop();
+
+    }
+
+    @Override
+    public void enterLoop(DhParser.LoopContext ctx) {
+        //System.out.println("enter loop" + ctx);
+       
+    }
+
+    @Override
+    public void exitLoop(DhParser.LoopContext ctx) {
         System.out.println("exit loop" + ctx);
     }
-    /**
-     * {@inheritDoc}
-     *
-     * <p>The default implementation does nothing.</p>
-     */
-    @Override public void enterPrint(DhParser.PrintContext ctx) { }
-    /**
-     * {@inheritDoc}
-     *
-     * <p>The default implementation does nothing.</p>
-     */
-    @Override public void exitPrint(DhParser.PrintContext ctx) { }
-    /**
-     * {@inheritDoc}
-     *
-     * <p>The default implementation does nothing.</p>
-     */
-    @Override public void enterExpr(DhParser.ExprContext ctx) { }
-    /**
-     * {@inheritDoc}
-     *
-     * <p>The default implementation does nothing.</p>
-     */
-    @Override public void exitExpr(DhParser.ExprContext ctx) { }
-    /**
-     * {@inheritDoc}
-     *
-     * <p>The default implementation does nothing.</p>
-     */
-    @Override public void enterAtomExpr(DhParser.AtomExprContext ctx) { }
-    /**
-     * {@inheritDoc}
-     *
-     * <p>The default implementation does nothing.</p>
-     */
-    @Override public void exitAtomExpr(DhParser.AtomExprContext ctx) { }
-    /**
-     * {@inheritDoc}
-     *
-     * <p>The default implementation does nothing.</p>
-     */
-    @Override public void enterAdd(DhParser.AddContext ctx) { }
-    /**
-     * {@inheritDoc}
-     *
-     * <p>The default implementation does nothing.</p>
-     */
-    @Override public void exitAdd(DhParser.AddContext ctx) { }
 
-    /**
-     * {@inheritDoc}
-     *
-     * <p>The default implementation does nothing.</p>
-     */
-    @Override public void enterEveryRule(ParserRuleContext ctx) { }
-    /**
-     * {@inheritDoc}
-     *
-     * <p>The default implementation does nothing.</p>
-     */
-    @Override public void exitEveryRule(ParserRuleContext ctx) { }
-    /**
-     * {@inheritDoc}
-     *
-     * <p>The default implementation does nothing.</p>
-     */
-    @Override public void visitTerminal(TerminalNode node) { }
-    /**
-     * {@inheritDoc}
-     *
-     * <p>The default implementation does nothing.</p>
-     */
-    @Override public void visitErrorNode(ErrorNode node) { }
+    @Override
+    public void enterPrint(DhParser.PrintContext ctx) { }
+
+    @Override
+    public void exitPrint(DhParser.PrintContext ctx) {
+
+        System.out.println(stack.pop());
+
+    }
+
+    @Override
+    public void enterExpr(DhParser.ExprContext ctx) { }
+
+    @Override
+    public void exitExpr(DhParser.ExprContext ctx) {
+
+    }
+
+    @Override
+    public void enterAtomExpr(DhParser.AtomExprContext ctx) {
+
+        if (ctx.ID() != null) {
+
+            stack.push(getVar(ctx.ID().getSymbol()).val);
+
+        } else if (ctx.INT() != null) {
+
+            stack.push(Integer.parseInt(ctx.INT().getText()));
+        }
+    }
+
+    @Override
+    public void exitAtomExpr(DhParser.AtomExprContext ctx) { }
+
+    @Override
+    public void enterAdd(DhParser.AddContext ctx) { }
+
+    @Override
+    public void exitAdd(DhParser.AddContext ctx) {
+
+        if (ctx.expr() != null) {
+
+            stack.push(stack.pop() + stack.pop());
+        }
+    }
 
 }
